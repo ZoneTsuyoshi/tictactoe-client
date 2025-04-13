81fff8a52346e4fd73ae995ce140dca2d19a356f
ZoneTsuyoshi <5413.6423.87896744.47449663.ti@gmail.com>
2025-04-13 11:01:17 +0900

    check i/e prob

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Client } from 'boardgame.io/react';
 import { TicTacToe } from './game';
-import { Board } from './board';
+// import { Board } from './board';
+
+const Board = ({ G, ctx, moves }) => {
+  return (
+    <div>
+      <h2>Player {ctx.currentPlayer}</h2>
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 100px)' }}>
+        {G.cells.map((cell, i) => (
+          <button
+            key={i}
+            onClick={() => moves.clickCell(i)}
+            style={{ width: 100, height: 100, fontSize: 24 }}
+          >
+            {cell}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const App = Client({
   game: TicTacToe,
